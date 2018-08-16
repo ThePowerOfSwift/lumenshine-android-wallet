b449b3040e3a7973f25708f2e741decca3ff63f9
janosiatti <attti90@gmail.com>
2018-08-16 22:59:52 +0300

    -in 2FA confirmation: removed QR image, and fixed 2FA secret copy, fixed error handling

--- app/src/main/java/com/soneso/lumenshine/presentation/auth/TfaConfirmationFragment.kt
+++ app/src/main/java/com/soneso/lumenshine/presentation/auth/TfaConfirmationFragment.kt
@@ -4,15 +4,16 @@ package com.soneso.lumenshine.presentation.auth
 import android.arch.lifecycle.Observer
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.authenticator.OtpProvider
 import com.soneso.lumenshine.R
+import com.soneso.lumenshine.domain.data.ErrorCodes
+import com.soneso.lumenshine.domain.data.RegistrationStatus
+import com.soneso.lumenshine.domain.data.SgError
 import com.soneso.lumenshine.domain.data.TfaSecret
 import com.soneso.lumenshine.presentation.general.SgViewState
 import com.soneso.lumenshine.presentation.general.State
@@ -40,6 +41,10 @@ class TfaConfirmationFragment : AuthFragment() {
             renderTfaSecret(it ?: return@Observer)
         })
 
+        authViewModel.liveRegistrationStatus.observe(this, Observer {
+            renderRegistrationStatus(it ?: return@Observer)
+        })
+
     }
 
     private fun setupListeners() {
@@ -61,35 +66,74 @@ class TfaConfirmationFragment : AuthFragment() {
 
         when (viewState.state) {
             State.ERROR -> {
-
-                showErrorSnackbar(viewState.error)
+                handleError(viewState.error)
             }
             State.LOADING -> {
             }
             State.READY -> {
-
                 setupToken(viewState.data!!)
+            }
+        }
+    }
+
+    private fun renderRegistrationStatus(viewState: SgViewState<RegistrationStatus>) {
+        when (viewState.state) {
+            State.ERROR -> {
+                showLoadingButton(false)
+                handleError(viewState.error)
+            }
+            State.LOADING -> {
+                showLoadingButton(true)
+            }
+            State.READY -> {
+                showLoadingButton(false)
+            }
+        }
+    }
+
+    private fun showLoadingButton(loading: Boolean) {
+        if (loading) {
+            change_tfa_progress.visibility = View.VISIBLE
+            send_button.visibility = View.INVISIBLE
+        } else {
+            change_tfa_progress.visibility = View.GONE
+            send_button.visibility = View.VISIBLE
+        }
+    }
+
+    /**
+     * handling response errors
+     */
+    private fun handleError(e: SgError?) {
+        val error = e ?: return
+
+        when (error.errorCode) {
+            ErrorCodes.LOGIN_INVALID_2FA -> {
+                tfa_code_view.error = if (error.errorResId == 0) error.message!! else getString(error.errorResId)
+            }
+            else -> {
+                showErrorSnackbar(error)
             }
         }
     }
 
     private fun setupToken(tfaSecret: TfaSecret) {
 
-        qr_code_view.post {
-            val params = qr_code_view.layoutParams
-            params.height = qr_code_view.width
-            qr_code_view.requestLayout()
-
-            qr_code_view.setImageBitmap(BitmapFactory.decodeByteArray(tfaSecret.imageData, 0, tfaSecret.imageData.size))
-        }
+//        qr_code_view.post {
+//            val params = qr_code_view.layoutParams
+//            params.height = qr_code_view.width
+//            qr_code_view.requestLayout()
+//
+//            qr_code_view.setImageBitmap(BitmapFactory.decodeByteArray(tfaSecret.imageData, 0, tfaSecret.imageData.size))
+//        }
         token_view.keyListener = null
         token_view.text = tfaSecret.secretCode
         copy_button.setOnClickListener {
 
             val clipboard = context?.getSystemService(AppCompatActivity.CLIPBOARD_SERVICE) as ClipboardManager
-            val clip = ClipData.newPlainText("", OtpProvider.currentTotpCode(tfaSecret.secretCode))
+            val clip = ClipData.newPlainText("", tfaSecret.secretCode)
             clipboard.primaryClip = clip
-            showSnackbar("Token copied in clipboard!")
+            showSnackbar(getString(R.string.secret_copied))
         }
     }
 
